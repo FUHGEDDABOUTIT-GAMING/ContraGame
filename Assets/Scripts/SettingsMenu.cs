9a72567224d88052adbf3a2b490a5e79e508f555
Echochris234 <christianvargas1595@gmail.com>
2020-12-14 18:41:09 -0500

    Settings menu added still sloppy// routes work

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SettingsMenu: MonoBehaviour
+{
+    public string gameSceneName;
+    public void closeSettings()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
