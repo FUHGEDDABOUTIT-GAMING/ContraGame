9fe31ed371b5c6cd606245d17813d3bb88e2171b
Echochris234 <christianvargas1595@gmail.com>
2020-12-14 18:46:33 -0500

    GameOver added needs to be implemented

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public void playAgain(){
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    
+    public void menu(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    
+}
