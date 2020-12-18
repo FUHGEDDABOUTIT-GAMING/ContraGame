8204d1a386b30b9f82e75f681cc9482518af4dfa
wen372 <wen372@gmail.com>
2020-12-18 00:13:42 -0500

    Added health bar

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour {
+
+	Vector3 localScale;
+
+	// Use this for initialization
+	void Start () {
+		localScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		localScale.x = ControlPlayer2D.health;
+		transform.localScale = localScale;
+	}
+}
